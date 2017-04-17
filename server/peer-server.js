c65a7d0d44f1de55d57aefa970a72b0a0319ea09
Josh Lloyd <joshua.dp.lloyd@gmail.com>
2017-04-17 14:26:51 +0000

    add peerserver

---
+++ server/peer-server.js
@@ -0,0 +1,11 @@
+//create instance of peer server with SSL encryptioy
+var fs = require('fs');
+var PeerServer = require('peer').PeerServer;
+
+var server = PeerServer({
+  port: 9000,
+  ssl: {
+    key: fs.readFileSync('/path/to/your/ssl/key/here.key'),
+    cert: fs.readFileSync('/path/to/your/ssl/certificate/here.crt')
+  }
+});
