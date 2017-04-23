c9cbb4300a764041ad3ebc6da6ffc1da689bf0ae
Josh Lloyd <joshua.dp.lloyd@gmail.com>
2017-04-23 14:55:19 +0000

    incorporate express js

--- public/client.js
+++ public/client.js
@@ -12,5 +12,6 @@ peer.on('open', function(id) {
 });
 
 // print number of peers to console
-var numberOfPeers = peerList.length;
-console.log('Number of peers contained in PeerServer: ' + numberOfPeers);
+
+// var numberOfPeers = peerList.length;
+// console.log('Number of peers contained in PeerServer: ' + numberOfPeers);
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var server = PeerServer({port: 8081, path: '/peerserver'});
 // When connection from peer is established, push id to list
 server.on('connection', function(id) { 
     peerList.push(id);
+    console.log(peerList.length);
 });
 
 // When peer disconnects from server or peer is no longer reachable
