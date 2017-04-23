b77cc4315c406937c0077fe95f0265916a0a960d
Josh Lloyd <joshua.dp.lloyd@gmail.com>
2017-04-23 12:56:02 +0000

    include peer js from cdn and local client scripts to index html

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
+// Declare list of peers
+var peerList = [];
+
+// Create a peer server listening on port 8081
 var PeerServer = require('peer').PeerServer;
 var server = PeerServer({port: 8081, path: '/peerserver'});
+
+// When connection from peer is established, push id to list
+server.on('connection', function(id) { 
+    peerList.push(id);
+});
