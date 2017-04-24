141e1954351ed421d0640b83a2025ec890d8d70a
Josh Lloyd <joshua.dp.lloyd@gmail.com>
2017-04-24 19:08:17 +0000

    manage connections to other peers

--- public/client.js
+++ public/client.js
@@ -6,12 +6,30 @@ var peer = new Peer({
         debug: 3                                     // debug info needed (3 = highest amount)
     });
 
+// create list of data connections
+var dataConnections = [];
+
 // print user id to console
 peer.on('open', function(id) {
   console.log('My peer ID is: ' + id);
 });
 
-// Get peerList from server
+// establish connection with all peers in list...
+// get peer list from server
 $.get("https://peerjs-test-69-joshlloyd.c9users.io:8081/peerlist").done(function (peerList) {
-    console.log('List of peers:', peerList.length);
+    // loop through peer list
+    for( var i=0; i<peerList.length; i++ ) {
+        // create data connection between local peer and current remote peer
+        var dataConn = peer.connect(i);
+        // push data connection to list of data connections associated with this peer if it don't already exist
+        dataConnections.indexOf(dataConn) === -1 ? dataConnections.push(dataConn) : console.log("This item already exists");
+    }
+});
+
+// whenever another peer attempts to connect with us
+peer.on('connection', function(conn) {
+    // create data connection between remote peer and local peer
+    var dataConn = peer.connect(conn);
+    // push data connection to list of data connections if it doesn't exist already
+    dataConnections.indexOf(dataConn) === -1 ? dataConnections.push(dataConn) : console.log("This item already exists");
 });
